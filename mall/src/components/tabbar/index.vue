41d1233c0951c4097de5f48b92af7ef4976e7c72
jingdd-boop <1736929925@qq.com>
2020-06-14 22:30:04 +0800

    vue4

---
+++ mall/src/components/tabbar/index.vue
@@ -0,0 +1,36 @@
+<template>
+ <div class="g-tabbar">
+ <router-link class="g-tabbar-item" to="/home">
+        <i class="iconfont ico-shouye">
+            <span>首页</span>
+        </i>
+    </router-link>
+     <router-link class="g-tabbar-item" to="/home">
+        <i class="iconfont ico-shouye">
+            <span>首页</span>
+        </i>
+    </router-link>
+     <router-link class="g-tabbar-item" to="/home">
+        <i class="iconfont ico-shouye">
+            <span>首页</span>
+        </i>
+    </router-link>
+     <router-link class="g-tabbar-item" to="/home">
+        <i class="iconfont ico-shouye">
+            <span>首页</span>
+        </i>
+    </router-link>
+ </div>
+   
+   
+</template>
+
+<script>
+export default {
+    name: 'CTabbar'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
